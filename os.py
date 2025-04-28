b3c0c8cdcf07de53b9c3ae1512d5ad8e32c99760
elmorsihemida <elmorsihemida@gmail.com>
2025-04-28 18:52:44 +0300

    OS Module Some Function

---
+++ os.py
@@ -0,0 +1,16 @@
+import os
+#=====> OS module functions
+#print(os.name)
+# print(os.cpu_count())
+# print(os.getcwd())
+# print(os.getenv)
+#os.mkdir("hello")
+#os.rmdir("hello")
+#print(os.listdir())
+print(os.getlogin())
+
+#=============> Runs Commands on the host system
+print(os.system('whoami'))
+os.listdir()["oldfile"]
+
+
